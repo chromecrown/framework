64f71edc6171ccb369619f6eef9983bb140be020
augneb <augneb@gmail.com>
2017-08-14 17:30:45 +0800

    bug fix

--- src/Model.php
+++ src/Model.php
@@ -2,6 +2,7 @@
 
 namespace Weipaitang\Framework;
 
+use Weipaitang\Container\ContainerInterface;
 use Weipaitang\Database\Model as AbstractModel;
 
 /**
@@ -11,4 +12,13 @@ use Weipaitang\Database\Model as AbstractModel;
 class Model extends AbstractModel
 {
     use TraitBase;
+
+    protected $container;
+
+    public function __construct(ContainerInterface $container)
+    {
+        $this->container = $container;
+
+        parent::__construct($container);
+    }
 }
